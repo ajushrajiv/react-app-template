7f35a7d047c781c93fb086866d0e6698f86d2cee
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-01-30 10:12:11 +0100

    navbar

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -6,7 +6,7 @@ import NavigationBar from './components/NavigationBar';
 function App() {
   return (
     <div className="App">
-       <NavigationBar/>
+          <NavigationBar/>
         <header className="App-header">    
         <img src={logo} className="App-logo" alt="logo" />
         <p>
